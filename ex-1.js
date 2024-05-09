3ab2e1536af83392656b522390fe4033426ad4ac
Moth <kampanee.moth@gmail.com>
2024-05-09 15:41:04 +0700

    test: add ex-1 to ex-3 .js

--- ex-1.js
+++ ex-1.js
@@ -4,5 +4,7 @@ let hasCriminalBlacklist;
 let isAllow;
 
 // Start coding here
-
+isOver18 = 18;
+hasCriminalBlacklist = false;
+isAllow = (isOver18 > 18) && (hasCriminalBlacklist == false);
 console.log(isAllow);
