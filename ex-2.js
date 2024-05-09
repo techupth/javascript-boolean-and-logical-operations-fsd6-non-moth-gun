33f2ab068b6fe8d1b0a11686fd08e0b386b5baf9
KeeratiNon <keeratinon3@gmail.com>
2024-05-09 14:06:56 +0700

    Answer-ex1-3

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,20 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = true;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = false;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = false;
 
-let hasPromotion;
+let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent) || (isPlatinum);
+
+console.log(hasPromotion);
+
+lastMonthPaidMoreThan4000 = 2>1;
+isWeekday = 3>1;
+hasBoughtProductFromITCategory = 2>3;
+hasAttendedDiscountEvent = 4>2;
+isPlatinum = 3>2;
 
 console.log(hasPromotion);
